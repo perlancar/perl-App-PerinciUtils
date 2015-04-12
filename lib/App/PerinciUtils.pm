fbeb38b4736343bece7260b610346ca8fced5e4d
perlancar (@pc-home) <perlancar@gmail.com>
2015-04-12 15:33:07 +0700

    Initial commit

---
+++ lib/App/PerinciUtils.pm
@@ -0,0 +1,23 @@
+package App::PerinciUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+
+1;
+#ABSTRACT: Some utilities related to Perinci
+
+=head1 DESCRIPTION
+
+This distribution includes a few utility scripts related to Perinci::* modules
+family.
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Perinci>
+
+=cut
